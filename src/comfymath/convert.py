b79362e2ed58d7e4e239ee29cec591e925e3e2da
= <=>
2024-08-27 16:07:26 +0800

    add save image for api

--- src/comfymath/convert.py
+++ src/comfymath/convert.py
@@ -46,80 +46,139 @@ class Textbox:
     def func(self, a: str) -> tuple[str]:
         return (a,)
 
-class LoadImage:
+# class LoadImage:
+#     @classmethod
+#     def INPUT_TYPES(s):
+#         input_dir = folder_paths.get_input_directory()
+#         files = [f for f in os.listdir(input_dir) if os.path.isfile(os.path.join(input_dir, f))]
+#         return {"required":
+#                     {"image": (sorted(files), {"image_upload": True})},
+#                 }
+
+#     CATEGORY = "image"
+
+#     RETURN_TYPES = ("IMAGE", "MASK")
+#     FUNCTION = "load_image"
+#     def load_image(self, image):
+#         image_path = folder_paths.get_annotated_filepath(image)
+        
+#         img = node_helpers.pillow(Image.open, image_path)
+        
+#         output_images = []
+#         output_masks = []
+#         w, h = None, None
+
+#         excluded_formats = ['MPO']
+        
+#         for i in ImageSequence.Iterator(img):
+#             i = node_helpers.pillow(ImageOps.exif_transpose, i)
+
+#             if i.mode == 'I':
+#                 i = i.point(lambda i: i * (1 / 255))
+#             image = i.convert("RGB")
+
+#             if len(output_images) == 0:
+#                 w = image.size[0]
+#                 h = image.size[1]
+            
+#             if image.size[0] != w or image.size[1] != h:
+#                 continue
+            
+#             image = np.array(image).astype(np.float32) / 255.0
+#             image = torch.from_numpy(image)[None,]
+#             if 'A' in i.getbands():
+#                 mask = np.array(i.getchannel('A')).astype(np.float32) / 255.0
+#                 mask = 1. - torch.from_numpy(mask)
+#             else:
+#                 mask = torch.zeros((64,64), dtype=torch.float32, device="cpu")
+#             output_images.append(image)
+#             output_masks.append(mask.unsqueeze(0))
+
+#         if len(output_images) > 1 and img.format not in excluded_formats:
+#             output_image = torch.cat(output_images, dim=0)
+#             output_mask = torch.cat(output_masks, dim=0)
+#         else:
+#             output_image = output_images[0]
+#             output_mask = output_masks[0]
+
+#         return (output_image, output_mask)
+
+#     @classmethod
+#     def IS_CHANGED(s, image):
+#         image_path = folder_paths.get_annotated_filepath(image)
+#         m = hashlib.sha256()
+#         with open(image_path, 'rb') as f:
+#             m.update(f.read())
+#         return m.digest().hex()
+
+#     @classmethod
+#     def VALIDATE_INPUTS(s, image):
+#         if not folder_paths.exists_annotated_filepath(image):
+#             return "Invalid image file: {}".format(image)
+
+#         return True
+
+
+class SaveImage:
+    def __init__(self):
+        self.output_dir = folder_paths.get_output_directory()
+        self.type = "output"
+        self.prefix_append = ""
+        self.compress_level = 4
+        self.is_changed = True
+
     @classmethod
     def INPUT_TYPES(s):
-        input_dir = folder_paths.get_input_directory()
-        files = [f for f in os.listdir(input_dir) if os.path.isfile(os.path.join(input_dir, f))]
-        return {"required":
-                    {"image": (sorted(files), {"image_upload": True})},
+        return {"required": 
+                    {"images": ("IMAGE", ),
+                     "filename_prefix": ("STRING", {"default": "ComfyUI"})},
+                "hidden": {"prompt": "PROMPT", "extra_pnginfo": "EXTRA_PNGINFO"},
                 }
+
+    RETURN_TYPES = ()
+    FUNCTION = "save_images"
+
+    OUTPUT_NODE = True
 
     CATEGORY = "image"
 
-    RETURN_TYPES = ("IMAGE", "MASK")
-    FUNCTION = "load_image"
-    def load_image(self, image):
-        image_path = folder_paths.get_annotated_filepath(image)
-        
-        img = node_helpers.pillow(Image.open, image_path)
-        
-        output_images = []
-        output_masks = []
-        w, h = None, None
-
-        excluded_formats = ['MPO']
-        
-        for i in ImageSequence.Iterator(img):
-            i = node_helpers.pillow(ImageOps.exif_transpose, i)
-
-            if i.mode == 'I':
-                i = i.point(lambda i: i * (1 / 255))
-            image = i.convert("RGB")
-
-            if len(output_images) == 0:
-                w = image.size[0]
-                h = image.size[1]
-            
-            if image.size[0] != w or image.size[1] != h:
-                continue
-            
-            image = np.array(image).astype(np.float32) / 255.0
-            image = torch.from_numpy(image)[None,]
-            if 'A' in i.getbands():
-                mask = np.array(i.getchannel('A')).astype(np.float32) / 255.0
-                mask = 1. - torch.from_numpy(mask)
-            else:
-                mask = torch.zeros((64,64), dtype=torch.float32, device="cpu")
-            output_images.append(image)
-            output_masks.append(mask.unsqueeze(0))
-
-        if len(output_images) > 1 and img.format not in excluded_formats:
-            output_image = torch.cat(output_images, dim=0)
-            output_mask = torch.cat(output_masks, dim=0)
-        else:
-            output_image = output_images[0]
-            output_mask = output_masks[0]
-
-        return (output_image, output_mask)
-
     @classmethod
-    def IS_CHANGED(s, image):
-        image_path = folder_paths.get_annotated_filepath(image)
-        m = hashlib.sha256()
-        with open(image_path, 'rb') as f:
-            m.update(f.read())
-        return m.digest().hex()
+    def IS_CHANGED(s, latent):
+        # 强制刷新，这在api请求时候总会在ws返回executed 事件。
+        self.is_changed = not self.is_changed
+        return self.is_changed
 
-    @classmethod
-    def VALIDATE_INPUTS(s, image):
-        if not folder_paths.exists_annotated_filepath(image):
-            return "Invalid image file: {}".format(image)
+    def save_images(self, images, filename_prefix="ComfyUI", prompt=None, extra_pnginfo=None):
+        filename_prefix += self.prefix_append
+        full_output_folder, filename, counter, subfolder, filename_prefix = folder_paths.get_save_image_path(filename_prefix, self.output_dir, images[0].shape[1], images[0].shape[0])
+        results = list()
+        for (batch_number, image) in enumerate(images):
+            i = 255. * image.cpu().numpy()
+            img = Image.fromarray(np.clip(i, 0, 255).astype(np.uint8))
+            metadata = None
+            if not args.disable_metadata:
+                metadata = PngInfo()
+                if prompt is not None:
+                    metadata.add_text("prompt", json.dumps(prompt))
+                if extra_pnginfo is not None:
+                    for x in extra_pnginfo:
+                        metadata.add_text(x, json.dumps(extra_pnginfo[x]))
 
-        return True
+            filename_with_batch_num = filename.replace("%batch_num%", str(batch_number))
+            file = f"{filename_with_batch_num}_{counter:05}_.png"
+            
+            img.save(os.path.join(full_output_folder, file), pnginfo=metadata, compress_level=self.compress_level)
+            results.append({
+                "filename": file,
+                "subfolder": subfolder,
+                "type": self.type
+            })
+            counter += 1
 
+        return { "ui": { "images": results } }
 
 NODE_CLASS_MAPPINGS = {
     "Text box": Textbox,
     "Text": Textbox,
+    "Pt-Save Image": SaveImage
 }
