4415e5a47e45e8a5c67a0bc62175437a8202df89
= <=>
2024-08-27 16:10:22 +0800

    add load image for api

--- src/comfymath/convert.py
+++ src/comfymath/convert.py
@@ -46,77 +46,84 @@ class Textbox:
     def func(self, a: str) -> tuple[str]:
         return (a,)
 
-# class LoadImage:
-#     @classmethod
-#     def INPUT_TYPES(s):
-#         input_dir = folder_paths.get_input_directory()
-#         files = [f for f in os.listdir(input_dir) if os.path.isfile(os.path.join(input_dir, f))]
-#         return {"required":
-#                     {"image": (sorted(files), {"image_upload": True})},
-#                 }
+class LoadImage:
+    @classmethod
+    def INPUT_TYPES(s):
+        input_dir = folder_paths.get_input_directory()
+        files = [f for f in os.listdir(input_dir) if os.path.isfile(os.path.join(input_dir, f))]
+        return {"required":
+                    {"image": (sorted(files), {"image_upload": True})},
+                }
 
-#     CATEGORY = "image"
+    CATEGORY = "image"
 
-#     RETURN_TYPES = ("IMAGE", "MASK")
-#     FUNCTION = "load_image"
-#     def load_image(self, image):
-#         image_path = folder_paths.get_annotated_filepath(image)
+    RETURN_TYPES = ("IMAGE", "MASK")
+    FUNCTION = "load_image"
+    def load_image(self, image):
+        if not folder_paths.exists_annotated_filepath(image):
+            import base64
+            from io import BytesIO
+            img = node_helpers.pillow(Image.open, BytesIO(base64.b64decode(image)))
+        else:
+            image_path = folder_paths.get_annotated_filepath(image)
+            img = node_helpers.pillow(Image.open, image_path)
         
-#         img = node_helpers.pillow(Image.open, image_path)
+        output_images = []
+        output_masks = []
+        w, h = None, None
+
+        excluded_formats = ['MPO']
         
-#         output_images = []
-#         output_masks = []
-#         w, h = None, None
+        for i in ImageSequence.Iterator(img):
+            i = node_helpers.pillow(ImageOps.exif_transpose, i)
 
-#         excluded_formats = ['MPO']
-        
-#         for i in ImageSequence.Iterator(img):
-#             i = node_helpers.pillow(ImageOps.exif_transpose, i)
+            if i.mode == 'I':
+                i = i.point(lambda i: i * (1 / 255))
+            image = i.convert("RGB")
 
-#             if i.mode == 'I':
-#                 i = i.point(lambda i: i * (1 / 255))
-#             image = i.convert("RGB")
-
-#             if len(output_images) == 0:
-#                 w = image.size[0]
-#                 h = image.size[1]
+            if len(output_images) == 0:
+                w = image.size[0]
+                h = image.size[1]
             
-#             if image.size[0] != w or image.size[1] != h:
-#                 continue
+            if image.size[0] != w or image.size[1] != h:
+                continue
             
-#             image = np.array(image).astype(np.float32) / 255.0
-#             image = torch.from_numpy(image)[None,]
-#             if 'A' in i.getbands():
-#                 mask = np.array(i.getchannel('A')).astype(np.float32) / 255.0
-#                 mask = 1. - torch.from_numpy(mask)
-#             else:
-#                 mask = torch.zeros((64,64), dtype=torch.float32, device="cpu")
-#             output_images.append(image)
-#             output_masks.append(mask.unsqueeze(0))
+            image = np.array(image).astype(np.float32) / 255.0
+            image = torch.from_numpy(image)[None,]
+            if 'A' in i.getbands():
+                mask = np.array(i.getchannel('A')).astype(np.float32) / 255.0
+                mask = 1. - torch.from_numpy(mask)
+            else:
+                mask = torch.zeros((64,64), dtype=torch.float32, device="cpu")
+            output_images.append(image)
+            output_masks.append(mask.unsqueeze(0))
 
-#         if len(output_images) > 1 and img.format not in excluded_formats:
-#             output_image = torch.cat(output_images, dim=0)
-#             output_mask = torch.cat(output_masks, dim=0)
-#         else:
-#             output_image = output_images[0]
-#             output_mask = output_masks[0]
+        if len(output_images) > 1 and img.format not in excluded_formats:
+            output_image = torch.cat(output_images, dim=0)
+            output_mask = torch.cat(output_masks, dim=0)
+        else:
+            output_image = output_images[0]
+            output_mask = output_masks[0]
 
-#         return (output_image, output_mask)
+        return (output_image, output_mask)
 
-#     @classmethod
-#     def IS_CHANGED(s, image):
-#         image_path = folder_paths.get_annotated_filepath(image)
-#         m = hashlib.sha256()
-#         with open(image_path, 'rb') as f:
-#             m.update(f.read())
-#         return m.digest().hex()
+    @classmethod
+    def IS_CHANGED(s, image):
+        m = hashlib.sha256()
+        if not folder_paths.exists_annotated_filepath(image):
+            m.update(base64.b64decode(image))
+        else:
+            image_path = folder_paths.get_annotated_filepath(image)   
+            with open(image_path, 'rb') as f:
+                m.update(f.read())
+        return m.digest().hex()
 
-#     @classmethod
-#     def VALIDATE_INPUTS(s, image):
-#         if not folder_paths.exists_annotated_filepath(image):
-#             return "Invalid image file: {}".format(image)
+    @classmethod
+    def VALIDATE_INPUTS(s, image):
+        # if not folder_paths.exists_annotated_filepath(image):
+        #     return "Invalid image file: {}".format(image)
 
-#         return True
+        return True
 
 
 class SaveImage:
@@ -180,5 +187,6 @@ class SaveImage:
 NODE_CLASS_MAPPINGS = {
     "Text box": Textbox,
     "Text": Textbox,
-    "Pt-Save Image": SaveImage
+    "Pt-Save Image": SaveImage,
+    "Pt-Load Image": LoadImage
 }
